272266534a743624f88f18c8cf8a3087c74e399c
damonleaf29 <dale3791@colorado.edu>
2023-10-15 21:42:25 -0600

    updated comments and readMe

--- CoordinatesAPI.py
+++ CoordinatesAPI.py
@@ -4,7 +4,7 @@ import sys
 #References: 
 # https://ipstack.com/blog/using-python-to-convert-ip-addresses-into-location-data
 # https://ipstack.com/documentation
-# https://medium.com/geekculture/how-to-dockerize-a-python-script-b0af3cd11fb0
+# https://medium.com/geekculture/how-to-dockerize-a-python-script-b0af3cd11fb0 #ran into issues setting up docker
 
 
 def getCoordinates(ip_address, api_key):
@@ -13,9 +13,9 @@ def getCoordinates(ip_address, api_key):
     data = response.json() #data is returned as a dictionary
     latitude = data["latitude"]
     longitude = data["longitude"]
-    return latitude, longitude  # Returns the lat and long as a tuple of Strings
+    return latitude, longitude  # Returns the lat and long as a tuple of Floats
 
 apiAccessKey = "61f6673544e69512bafda6c994bfd715" 
 userIP = sys.argv[1] #IP for now #66.45.44.0 Random Colorado IP
 output = getCoordinates(userIP, apiAccessKey)
-print(output) # output is a tuple of Strings
+print(output) # output is a tuple of Floats
--- README.md
+++ README.md
@@ -7,3 +7,7 @@ python -m pip install requests
 Run using command:
 python3 CoordinatesAPI.py desiredIP  
 EX: python3 CoordinatesAPI.py 66.45.44.0  // A random Colorado IP
+
+Output:
+The output of the getCoordinates function is a tuple of Floats. (latitude, longitude)
+As for security, there are currently no checks for kinds of IP addresses we're recieving and if they're harmful or not. We could enable the security module in our get request by appending '&security = {1}', as recommended by https://ipstack.com/documentation.
