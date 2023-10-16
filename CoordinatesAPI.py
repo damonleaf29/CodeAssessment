d54cf7085429f3c4f198358fcebc56fca18d3e4a
damonleaf29 <dale3791@colorado.edu>
2023-10-15 20:51:56 -0600

    API request returns dictionary containing lat and long.

---
+++ CoordinatesAPI.py
@@ -0,0 +1,21 @@
+import requests
+import sys
+
+#References: 
+# https://ipstack.com/blog/using-python-to-convert-ip-addresses-into-location-data
+# https://ipstack.com/documentation
+# https://medium.com/geekculture/how-to-dockerize-a-python-script-b0af3cd11fb0
+
+
+def getCoordinates(ip_address, api_key):
+    base_url = "http://api.ipstack.com/"
+    response = requests.get(f"{base_url}{ip_address}?access_key={api_key}&fields={'latitude,longitude'}")
+    data = response.json()
+    latitude = data["latitude"]
+    longitude = data["longitude"]
+    return latitude, longitude  # Returns the lat and long as a tuple of Strings
+
+apiAccessKey = "61f6673544e69512bafda6c994bfd715" 
+userIP = sys.argv[1] #IP for now #66.45.44.0 Random Colorado IP
+output = getCoordinates(userIP, apiAccessKey)
+print(output) # output is a tuple of Strings
